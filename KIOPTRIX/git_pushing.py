c0f521d02f8ebb721de998b50830133b4dc0525e
root <root@kali.kali>
2022-11-01 20:48:43 -0700

    update write-up

---
+++ KIOPTRIX/git_pushing.py
@@ -0,0 +1,14 @@
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Git pushing script')
+parser.add_argument("-m", "--message", help="commit message", required=False)
+args = parser.parse_args()
+
+
+message = args.message if args.message else "update write-up"
+os.system('git add .')
+os.system("git commit -m '{}'".format(message))
+os.system('git push')
+
