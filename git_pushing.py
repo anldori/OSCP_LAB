f25d15d9b6a0203de0f92e2846a41f593f34e857
An Le Duy <anleduyact@gmail.com>
2022-12-08 14:29:03 +0700

    update write-up

--- git_pushing.py
+++ git_pushing.py
@@ -9,6 +9,6 @@ args = parser.parse_args()
 
 message = args.message if args.message else "update write-up"
 os.system('git add .')
-os.system("git commit -m '{}'".format(message))
+os.system("git commit -m \"{}\"".format(message))
 os.system('git push')
 
